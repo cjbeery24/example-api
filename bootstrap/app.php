e943b5e5f5eb903bfc5553843b888eeecbcafdd4
Caleb Beery <cjbeery@gmail.com>
2024-05-30 07:22:29 -0700

    Fixed pagination middleware.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\App\Http\Middleware\PaginateAPI::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api_v2.php
+++ routes/api_v2.php
@@ -11,15 +11,20 @@ Route::prefix('auth')->group(function() {
     Route::post('/login', [AuthController::class, 'login']);
     Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 });
-Route::middleware('auth:sanctum')->apiResource('ag_platformGroups', PlatformGroupController::class)->parameters([
-    'ag_platformGroups' => 'platformGroup'
-]);
-Route::middleware('auth:sanctum')->apiResource('ag_platforms', PlatformController::class)->parameters([
-    'ag_platforms' => 'platform'
-]);
-Route::middleware('auth:sanctum')->apiResource('ag_platformPositions', PlatformPositionController::class)->parameters([
-    'ag_platformPositions' => 'platformPosition'
-]);
+Route::middleware([
+    \App\Http\Middleware\PaginateAPI::class,
+    'auth:sanctum'
+])->group(function() {
+    Route::apiResource('ag_platformGroups', PlatformGroupController::class)->parameters([
+        'ag_platformGroups' => 'platformGroup'
+    ]);
+    Route::apiResource('ag_platforms', PlatformController::class)->parameters([
+        'ag_platforms' => 'platform'
+    ]);
+    Route::apiResource('ag_platformPositions', PlatformPositionController::class)->parameters([
+        'ag_platformPositions' => 'platformPosition'
+    ]);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
